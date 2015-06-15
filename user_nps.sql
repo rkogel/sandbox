7cbbac04431ffa052940358b14c02371cb84fc8d
Ruben Kogel <rubenkogel@Rubens-MacBook-Air.local>
2015-06-15 11:44:00 -0700

    modified logic for joining user_enrollment table so that only 1 entry is recorded

--- user_nps.sql
+++ user_nps.sql
@@ -8,22 +8,30 @@ select ucf.courseid,
 			 course.userid as instructorid,
 			 course.ispremium,
 			 case when ucl.oauth2clientid =1 then 'web' when ucl.oauth2clientid =5 then 'iOS app' when ucl.oauth2clientid =202 then 'Android app' else 'mini-apps & other' end as device,
-			 sum(ue.pricepaid) as pricepaid
-from user_course_feedback ucf
+			 ue.paid_amount as pricepaid
 
-inner join course on course.id = ucf.courseid and course.sourceorganizationid is null
-left  join course_has_subcategory chs on chs.courseid = course.id and chs.isprimary = 'true'
-left  join course_subcategory cs on cs.id = chs.coursesubcategoryid
-left  join course_category cc on cc.id = cs.coursecategoryid
+from raw_data.user_course_feedback ucf
 
-inner join user_enrollment ue on ue.userid = ucf.userid
-															and ue.courseid = ucf.courseid
-															and ue.channel != 'external_partner'
-															and date > '2014-1-1'
-															and (ue.channel != 'instructor' or ue.pricepaid >0)
+inner join raw_data.course on course.id = ucf.courseid and course.sourceorganizationid is null
+left  join raw_data.course_has_subcategory chs on chs.courseid = course.id and chs.isprimary = 'true'
+left  join raw_data.course_subcategory cs on cs.id = chs.coursesubcategoryid
+left  join raw_data.course_category cc on cc.id = cs.coursecategoryid
 
-left  join user_completed_lecture ucl on ucl.courseid = ucf.courseid and ucl.userid = ucf.userid and ucl.lectureid = ucf.sourcelectureid
+left  join raw_data.user_completed_lecture ucl on ucl.courseid = ucf.courseid and ucl.userid = ucf.userid and ucl.lectureid = ucf.sourcelectureid
+
+-- revert back to joining ue directly onto ucf once the uniqueness issue has been solved and move the filters out of the subquery
+inner join 
+	(
+		select userid,
+					 courseid,
+					 sum(paid_amount) as paid_amount,
+					 max(case when course_was_premium =true then 1 else 0 end) as course_was_premium
+		from analytics.user_enrollment
+		where traffic_channel != 'external_partner' and (traffic_channel != 'instructor' or paid_amount >0) and created > '2014-1-1'
+		group by 1,2
+	) ue
+on ue.userid = ucf.userid and ue.courseid = ucf.courseid
 
 where ucf.score is not null and ucf.wascourseprivate =0
 
-group by 1,2,3,4,5,6,7,8,9,10
+;
