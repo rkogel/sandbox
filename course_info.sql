0d40b86b20e55bef916920a4908031bc45a27707
Ruben Kogel <rubenkogel@Rubens-MacBook-Air.local>
2015-06-17 20:59:44 -0700

    routine update

--- course_info.sql
+++ course_info.sql
@@ -1,7 +1,7 @@
-select c.id as courseid,
+select distinct c.id as courseid,
 			 c.title,
 			 c.userid,
-			 u.title,
+			 u.title as name,
 			 c.created,
 			 case when cc.title is null then 'NA' else cc.title end as category,
 			 case when cs.title is null then 'NA' else cs.title end as subcategory,
@@ -36,20 +36,23 @@ select c.id as courseid,
 			 rat.reviewavg,
 			 rat.reviewcount,
 			 
-			 con.avg_consumption
+			 con.avg_con_first7d,
+			 con.num_viewers,
+
+			 duration.hrs as hrs_of_content
 			 
-from course c
+from raw_data.course c
 
-inner join u_user u on u.id = c.userid
+inner join raw_data.u_user u on u.id = c.userid
 
-left join course_has_subcategory chs on chs.courseid = c.id and chs.isprimary = 'true'
-left join course_subcategory cs on cs.id = chs.coursesubcategoryid
-left join course_category cc on cc.id = cs.coursecategoryid
+left join raw_data.course_has_subcategory chs on chs.courseid = c.id and chs.isprimary = 'true'
+left join raw_data.course_subcategory cs on cs.id = chs.coursesubcategoryid
+left join raw_data.course_category cc on cc.id = cs.coursecategoryid
 
 left join
 	(
 	select courseid, min(created)
-	from course_admin_rating_log
+	from raw_data.course_admin_rating_log
 	where adminrating >=7
 	group by 1
 	) cal on cal.courseid = c.id
@@ -57,18 +60,18 @@ left join
 left join
 	(
 	select courseid, min(modified)
-	from course_quality_historicalqualityreviewprocess
+	from raw_data.course_quality_historicalqualityreviewprocess
 	where status = 'approved' and scorecardid != 10
 	group by 1
 	)qrph
 on qrph.courseid = c.id
 
-left join course_quality_historicalqualityreviewprocess qrph2 on qrph2.courseid = qrph.courseid and qrph2.modified = qrph.min
+left join raw_data.course_quality_historicalqualityreviewprocess qrph2 on qrph2.courseid = qrph.courseid and qrph2.modified = qrph.min
 
 left join
 	(
 	select courseid, min(created)
-	from course_history
+	from raw_data.course_history
 	where actiontype = 'publish-course'
 	group by 1
 	) ch
@@ -85,13 +88,14 @@ left join
     from
       (
         select courseid, date, score, rank() over(partition by courseid order by date desc)
-        from user_nps
+        from analytics.user_nps
       )
     where rank <=250 or date >current_date - interval ' 3 month'
     group by 1 having count(1) >=20
   ) nps
 on nps.courseid = c.id
 
+-- change back user_enrollment after uniqueness issue has been resolved
 -- revenue + enrollment
 left join
 	(
@@ -100,8 +104,16 @@ left join
 				 count(case when paid_amount >0 then 1 end) as paidenrollment,
 				 sum(paid_amount) as totalrevenue,
 				 sum(case when created > current_date - interval '30 day' then paid_amount else 0 end) as revenue30d
-	from user_enrollment
-	where traffic_channel != 'external_partner'
+	from 
+		(
+			select userid,
+						 courseid,
+						 sum(paid_amount) as paid_amount,
+						 min(created) as created
+			from analytics.user_enrollment
+			where traffic_channel != 'external_partner'
+			group by 1,2
+		)
 	group by 1
 	) ue
 on ue.courseid = c.id
@@ -137,14 +149,42 @@ on rat.courseid = c.id
 -- avg consumption of paid students
 left join
 	(
-	select udc.courseid, sum(udc.minconsumed)/count(distinct udc.userid) as avg_consumption
-	from user_daily_course_consumption udc, user_enrollment ue
-	where ue.paid_amount >0
-	and ue.userid = udc.userid and ue.courseid = udc.courseid
-	group by 1
+		select udc.courseid,
+					 sum(udc.minconsumed)/count(distinct udc.userid) as avg_con_first7d,
+					 count(distinct udc.userid) as num_viewers
+		from 
+			(
+				select userid,
+							 courseid,
+							 minconsumed,
+							 min(date) over(partition by userid, courseid),
+					 		 datediff(day, min(date) over (partition by userid, courseid), date) as delay
+				from user_daily_course_consumption
+			) udc,
+			(
+				select userid,
+							 courseid,
+							 sum(paid_amount) as paid_amount,
+							 max(case when course_was_premium =true then 1 else 0 end) as course_was_premium
+				from analytics.user_enrollment
+				where traffic_channel != 'external_partner' and created > '2014-1-1'
+				group by 1,2
+			)	ue
+		where udc.min < current_date - interval '7 day' and udc.delay <7
+		and ue.userid = udc.userid and ue.courseid = udc.courseid and (ue.paid_amount >0 or ue.course_was_premium =0) -- change to false
+		group by 1 having count(distinct udc.userid) >20
 	) con
 on con.courseid = c.id
 
-where c.publishedtime is not null
+-- course duration
+left join
+	(
+	select cha.courseid, sum(a.length)*1.00/3600 as hrs
+	from course_has_asset cha, asset a
+	where a.type = 'Video'
+	and cha.assetid = a.id
+	group by 1
+	) duration
+on duration.courseid = c.id
 
-group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31
+where c.publishedtime is not null
