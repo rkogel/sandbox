56f3283cb65ba7501113b63b3c1ccade2b4d323c
Ruben Kogel <rubenkogel@Rubens-MacBook-Air.local>
2015-06-19 02:09:11 -0700

    v2.1 corrected earlier mistakes and tested new algo vs. old one

--- predicted_nps.sql
+++ predicted_nps.sql
@@ -27,41 +27,32 @@ con as
 					 		 datediff(day, min(date) over (partition by userid, courseid), date) as delay
 				from analytics.user_daily_course_consumption
 			) udc,
-			(
-				select userid,
-							 courseid,
-							 sum(paid_amount) as paid_amount,
-							 max(case when course_was_premium =true then 1 else 0 end) as course_was_premium
-				from analytics.user_enrollment
-				where traffic_channel != 'external_partner' and created > '2014-1-1'
-				group by 1,2
-			)	ue,
+			analytics.user_enrollment ue,
 			course_info ci
-		where udc.min < current_date - interval '7 day' and udc.delay <7 and udc.min > current_date - interval '3 month'
-		and ue.userid = udc.userid and ue.courseid = udc.courseid and (ue.paid_amount >0 or ue.course_was_premium =0) -- change to false
+		where ue.traffic_channel != 'external_partner' and ue.created > '2014-1-1' and ue.is_refund=false
+		and udc.min < current_date - interval '7 day' and udc.delay <7 and udc.min > current_date - interval '3 month'
+		and ue.userid = udc.userid and ue.courseid = udc.courseid and (ue.paid_amount >0 or ue.course_was_premium =0)
 		and ci.courseid = udc.courseid
 		group by 1
 	)
 
 select a.courseid,
-			 (coalesce(a.nps_qscore*erf_qscore.odd,0) + a.nps_con*erf_con.odd + coalesce(a.nps_review*erf_review.odd,0) + coalesce(a.raw_nps*erf_nps.odd,0))/(coalesce(erf_qscore.odd,0) + erf_con.odd + coalesce(erf_review.odd,0) + coalesce(erf_nps.odd,0)) as predicted_nps
+			 ((case when a.nps_qscore is null then a.nps_con else (a.nps_qscore + a.nps_con)*0.5 end)*erf_con.odd + coalesce(a.nps_review*erf_review.odd,0) + coalesce(a.raw_nps*erf_nps.odd,0))/(erf_con.odd + coalesce(erf_review.odd,0) + coalesce(erf_nps.odd,0)) as predicted_nps
 from
 	(
 		select ci.courseid,
-					 31 + qualityscore*.14 as nps_qscore,
-					 case when qualityscore is not null then 21.6 end as nps_qscore_error,
-					 case when subcat.nps is null then cat.nps else subcat.nps end + coalesce((0.5 - 0.2*case when ispremium in ('Yes', 'true') then 1 else 0 end)*(ci.avg_con_first7d - con.avg_con_first7d),0) as nps_con,
-					 case when ci.avg_con_first7d is null or con.avg_con_first7d is null then 21.6 else 20 end as nps_con_error,
-					 -150 + 40*ci.reviewavg as nps_review,
-					 26*power(ci.reviewcount, -.12) as nps_review_error,
+					 35 + ci.qualityscore*.14 as nps_qscore,
+					 case when subcat.nps is null then cat.nps else subcat.nps end + coalesce((0.4 - 0.15*case when ispremium in ('Yes', 'true') then 1 else 0 end)*(ci.avg_con_first7d - con.avg_con_first7d),0) as nps_con,
+					 case when ci.avg_con_first7d is null or con.avg_con_first7d is null then 17.8 else 16.5 end as nps_con_error,
+					 -100 + 30*ci.reviewavg as nps_review,
+					 case when reviewcount <45 then 22*power(reviewcount,-.1) else 14.8 end as nps_review_error,
 					 ci.nps as raw_nps,
-					 ci.precision as raw_nps_error
+					 78*power(npscount,-.5) as raw_nps_error
 		from course_info ci
 		left join subcat on subcat.subcategory = ci.subcategory
 		inner join cat on cat.category = ci.category
 		inner join con on con.subcategory = ci.subcategory
 	) a
-left join adhoc.error_function_lookup erf_qscore on erf_qscore.z100 = round(1000/a.nps_qscore_error)
 left join adhoc.error_function_lookup erf_con on erf_con.z100 = round(1000/a.nps_con_error)
 left join adhoc.error_function_lookup erf_review on erf_review.z100 = round(1000/a.nps_review_error)
 left join adhoc.error_function_lookup erf_nps on erf_nps.z100 = round(1000/a.raw_nps_error)
