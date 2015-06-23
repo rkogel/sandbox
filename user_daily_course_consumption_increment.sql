b8f9e3afb9281717f10b8db6852bbbbe580c4516
Ruben Kogel <rubenkogel@Rubens-MacBook-Air.local>
2015-06-23 09:40:34 -0700

    initial commit

---
+++ user_daily_course_consumption_increment.sql
@@ -0,0 +1,19 @@
+select userid,
+			 courseid,
+			 date(created),
+			 case when clientid =-2  then 'iOS Web'
+			 			when clientid =-2  then 'Android Web'
+				 		when clientid =1   then 'Web'
+				 		when clientid =5   then 'iOS'
+				 		when clientid =202 then 'Android' else 'mini-apps' end as device,
+			 case when rank=1 then true else false end as is_first_watch,
+			 isofflineprogress as is_offline,
+			 isbackgroundprogress as is_running_in_background,
+			 count(1)*.25 as minconsumed
+from
+	(
+		select *, rank() over(partition by userid, courseid, assetid order by created)
+		from user_progressed_lecture
+		where total >1 and created < current_date and created > (select max(date) from user_daily_course_consumption)
+	)
+group by 1,2,3,4,5,6,7
